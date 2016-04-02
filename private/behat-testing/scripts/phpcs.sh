fe01ec8b5984de9470bc234a4f3ee19e90173b39
Steve Persch <steve@stevepersch.com>
2016-04-02 12:10:11 -0500

    Adding themes to phpcs

--- private/behat-testing/scripts/phpcs.sh
+++ private/behat-testing/scripts/phpcs.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 ./bin/phpcs --report=full --extensions=php,module,inc,theme,info --standard=vendor/drupal/coder/coder_sniffer/Drupal/  ../../modules/custom 
-
+./bin/phpcs --report=full --extensions=php,module,inc,theme,info --standard=vendor/drupal/coder/coder_sniffer/Drupal/  ../../themes/custom
