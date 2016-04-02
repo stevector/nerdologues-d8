f8b6ebaed438a90aaac0fd5c567388b653b1c198
Steve Persch <steve@stevepersch.com>
2016-04-02 13:02:20 -0500

    Trying to get circle to fail

--- private/behat-testing/scripts/phpcs.sh
+++ private/behat-testing/scripts/phpcs.sh
@@ -1,3 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
+set -e
 ./bin/phpcs --report=full --extensions=php,module,inc,theme,info --standard=vendor/drupal/coder/coder_sniffer/Drupal/  ../../modules/custom 
 ./bin/phpcs --report=full --extensions=php,module,inc,theme,info --standard=vendor/drupal/coder/coder_sniffer/Drupal/  ../../themes/custom
