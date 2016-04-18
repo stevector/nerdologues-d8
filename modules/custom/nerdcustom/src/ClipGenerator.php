961b78b6ff0a0808c36ac3280b8c5a1f24bcbd36
Steve Persch <steve@stevepersch.com>
2016-04-17 19:20:46 -0500

    Code cleanup

--- modules/custom/nerdcustom/src/ClipGenerator.php
+++ modules/custom/nerdcustom/src/ClipGenerator.php
@@ -55,22 +55,48 @@ class ClipGenerator {
    *   The node being passed in from hook_entity_insert.
    */
   public function generateClips(NodeInterface $podcast_episode_node) {
+
+    if (!$this->validateEpisode($podcast_episode_node)) {
+      return;
+    }
+    if (empty($podcast_episode_node->field_body->getValue()[0]['value'])) {
+      return;
+    }
+    $body_text = $podcast_episode_node->field_body->getValue()[0]['value'];
+    foreach ($this->extractClipTitles($body_text) as $clip_title) {
+      // dsm($clip_title);
+    }
+  }
+
+  /**
+   * Validate that this node should have clips created for it.
+   *
+   * @param NodeInterface $podcast_episode_node
+   *   The node being passed in from hook_entity_insert.
+   */
+  protected function validateEpisode(NodeInterface $podcast_episode_node) {
+    if ($podcast_episode_node->bundle() === 'podcast_episode' && $podcast_episode_node->field_ref_podcast->referencedEntities()[0]->label() === 'Your Stories') {
+      return TRUE;
+    }
+    else {
+      return FALSE;
+    }
   }
 
   /**
    * Take a body field string and return all the potential clip titles in lis.
    *
-   * @param string $body_field
+   * @param string $body_text
    *   A node's body field.
    *
    * @return array
    *   An array of clip titles.
    */
-  public function extractClipTitles($body_field) {
+  public function extractClipTitles($body_text) {
     $return = [];
     // Clean problem tags and characters from a string that only needs to be
     // the lists.
-    $cleaned_body = strip_tags($body_field, '<ul><li>');
+    $cleaned_body = strip_tags($body_text, '<ul><li>');
     $cleaned_body = str_replace('&nbsp;', ' ', $cleaned_body);
     // Strip tags that aren't ul and li because font tags were causing errors.
     $xml = new SimpleXMLElement('<root>' . $cleaned_body . '</root>');
