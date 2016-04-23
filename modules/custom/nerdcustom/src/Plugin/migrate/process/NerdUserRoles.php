de8a89b701e726f8b3fddb8a84184a27b0c50d89
Steve Persch <steve@stevepersch.com>
2016-04-23 02:57:58 -0500

    gh10: adding NerdUserRoles.php

---
+++ modules/custom/nerdcustom/src/Plugin/migrate/process/NerdUserRoles.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Drupal\migrate\Plugin\migrate\process;
+
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\Row;
+
+/**
+ * This plugin sets missing values on the destination.
+ *
+ * @MigrateProcessPlugin(
+ *   id = "nerd_user_roles"
+ * )
+ */
+class NerdUserRoles extends ProcessPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+
+    if ($value == 3) {
+      return 'administrator';          
+    }
+    else if ($value == 4) {
+      return 'content_administrator';
+    }
+  }
+}
+
