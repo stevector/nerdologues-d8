946e936c334203db65a204d2c3c2c37ae0edf0cf
Steve Persch <steve@stevepersch.com>
2016-05-05 21:37:00 -0500

    Reading migration source from secrets file

--- sites/default/settings.migrate.php
+++ sites/default/settings.migrate.php
@@ -1,12 +1,47 @@
 <?php
 
+/**
+ * Get secrets from secrets file.
+ *
+ * @param array $requiredKeys  List of keys in secrets file that must exist.
+ */
+function _get_secrets($requiredKeys, $defaults)
+{
+  $secretsFile = $_SERVER['HOME'] . '/files/private/secrets.json';
+  if (!file_exists($secretsFile)) {
+    die('No secrets file found. Aborting!');
+  }
+  $secretsContents = file_get_contents($secretsFile);
+  $secrets = json_decode($secretsContents, 1);
+  if ($secrets == FALSE) {
+    die('Could not parse json in secrets file. Aborting!');
+  }
+  $secrets += $defaults;
+  $missing = array_diff($requiredKeys, array_keys($secrets));
+  if (!empty($missing)) {
+    die('Missing required keys in json secrets file: ' . implode(',', $missing) . '. Aborting!');
+  }
+  return $secrets;
+}
+
+$required_keys = [
+  'migrate_source_db__db_name',
+  'migrate_source_db__username',
+  'migrate_source_db__password',
+  'migrate_source_db__host',
+  'migrate_source_db__port'
+];
+
+$secrets = _get_secrets($required_keys, array());
+
+
 $databases['drupal_7']['default'] = array (
-   'database' => 'pantheon',
-   'username' => 'pantheon',
-   'password' => '35c194084b9b4c928531d407ad99a64f',
-   'host' => 'dbserver.migrateprep.90be258b-259e-4ea9-b674-6a35e65cc86d.drush.in',
-   'port' => '11545',
-   'driver' => 'mysql',
-   'prefix' => '',
-   'collation' => 'utf8mb4_general_ci',
+  'database' => $secrets['migrate_source_db__db_name'],
+  'username' => $secrets['migrate_source_db__username'],
+  'password' => $secrets['migrate_source_db__password'],
+  'host' => $secrets['migrate_source_db__host'],
+  'port' => $secrets['migrate_source_db__port'],
+  'driver' => 'mysql',
+  'prefix' => '',
+  'collation' => 'utf8mb4_general_ci',
 );
