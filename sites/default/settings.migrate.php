ab491b5bdfa25047c4f6aac813ee486dc4c96ee8
Steve Persch <stevector@users.noreply.github.com>
2016-05-14 21:55:26 -0500

    Simplifying secrets handling

--- sites/default/settings.migrate.php
+++ sites/default/settings.migrate.php
@@ -1,50 +1,17 @@
 <?php
 
-/**
- * Get secrets from secrets file.
- *
- * @param array $requiredKeys  List of keys in secrets file that must exist.
- */
-
-
-
-
-
-
-$required_keys = [
-  'migrate_source_db__db_name',
-  'migrate_source_db__username',
-  'migrate_source_db__password',
-  'migrate_source_db__host',
-  'migrate_source_db__port'
-];
-
-
-$defaults= array();
-$get_secrets = function ($required_keys, $defaults)
-{
+$get_secrets = function() {
   $secretsFile = $_SERVER['HOME'] . '/files/private/secrets.json';
   if (!file_exists($secretsFile)) {
-    die('No secrets file found. Aborting!');
+    return [];
   }
   $secretsContents = file_get_contents($secretsFile);
-  $secrets = json_decode($secretsContents, 1);
-  if ($secrets == FALSE) {
-    die('Could not parse json in secrets file. Aborting!');
-  }
-  $secrets += $defaults;
-  $missing = array_diff($required_keys, array_keys($secrets));
-  if (!empty($missing)) {
-    die('Missing required keys in json secrets file: ' . implode(',', $missing) . '. Aborting!');
-  }
-  return $secrets;
+  return json_decode($secretsContents, 1);
 };
 
 $secrets = $get_secrets($required_keys, $defaults);
 
-
 if (!empty($secrets['migrate_source_db__password'])) {
-
   $databases['drupal_7']['default'] = array (
     'database' => $secrets['migrate_source_db__db_name'],
     'username' => $secrets['migrate_source_db__username'],
@@ -55,7 +22,4 @@ if (!empty($secrets['migrate_source_db__password'])) {
     'prefix' => '',
     'collation' => 'utf8mb4_general_ci',
   );
-
-
-
 }
