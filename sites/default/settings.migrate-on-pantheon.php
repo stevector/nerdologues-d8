3495db91b73ae90e94c2c9ce3e2defe0a01cb7f7
Steve Persch <steve@stevepersch.com>
2016-05-15 01:57:06 -0500

    trying to get migration read

--- sites/default/settings.migrate-on-pantheon.php
+++ sites/default/settings.migrate-on-pantheon.php
@@ -1,16 +1,37 @@
 <?php
 
-$get_secrets = function() {
+/**
+ * Get secrets from secrets file.
+ *
+ * @param array $requiredKeys  List of keys in secrets file that must exist.
+ */
+$required_keys = [
+  'migrate_source_db__database',
+  'migrate_source_db__username',
+  'migrate_source_db__password',
+  'migrate_source_db__host',
+  'migrate_source_db__port'
+];
+$defaults= array();
+$get_secrets = function ($required_keys, $defaults)
+{
   $secretsFile = $_SERVER['HOME'] . '/files/private/secrets.json';
   if (!file_exists($secretsFile)) {
-    return [];
+    die('No secrets file found. Aborting!');
   }
   $secretsContents = file_get_contents($secretsFile);
-  return json_decode($secretsContents, 1);
+  $secrets = json_decode($secretsContents, 1);
+  if ($secrets == FALSE) {
+    die('Could not parse json in secrets file. Aborting!');
+  }
+  $secrets += $defaults;
+  $missing = array_diff($required_keys, array_keys($secrets));
+  if (!empty($missing)) {
+    die('Missing required keys in json secrets file: ' . implode(',', $missing) . '. Aborting!');
+  }
+  return $secrets;
 };
-
 $secrets = $get_secrets($required_keys, $defaults);
-
 if (!empty($secrets['migrate_source_db__password'])) {
   $databases['drupal_7']['default'] = array (
     'database' => $secrets['migrate_source_db__database'],
