e22a836db11b2abde4766ffa61188bdc004d51e5
Steve Persch <steve@stevepersch.com>
2016-07-09 23:53:16 -0500

    Plugin for dates

---
+++ modules/custom/nerdcustom/src/Plugin/migrate/process/NerdDatePublished.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * @file
+ * A process plugin to map old roles to new.
+ *
+ * Only nerdologues members should have bio pages.
+ */
+
+namespace Drupal\nerdcustom\Plugin\migrate\process;
+
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\Row;
+use DateTime;
+use DateTimeZone;
+
+/**
+ * This plugin maps old roles to new roles.
+ *
+ * @MigrateProcessPlugin(
+ *   id = "nerd_date_published"
+ * )
+ */
+class NerdDatePublished extends ProcessPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+      
+    $return  = str_replace(' ', 'T', $value['value']);
+    return $return;
+  }
+}
