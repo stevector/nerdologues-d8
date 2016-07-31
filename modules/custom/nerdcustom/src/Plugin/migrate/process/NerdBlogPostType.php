a42bb965261834fd1bbaa2a7ef7a03c1dceec591
Steve Persch <steve@stevepersch.com>
2016-07-31 18:08:02 -0500

    Blogpost migration plugin

---
+++ modules/custom/nerdcustom/src/Plugin/migrate/process/NerdBlogPostType.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @file
+ * This plugin maps "blogpost" to "blog_post"
+ */
+
+namespace Drupal\nerdcustom\Plugin\migrate\process;
+
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\Row;
+use Drupal\Core\Database\Database;
+
+
+/**
+ * This plugin maps "blogpost" to "blog_post"
+ *
+ * @MigrateProcessPlugin(
+ *   id = "nerd_blog_post_type"
+ * )
+ */
+class NerdBlogPostType extends ProcessPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+
+    return 'blog_post';
+  }
+}
