225e8dd2f8751753c0397cf2fe97f70f70e5ce49
Steve Persch <steve@stevepersch.com>
2016-09-03 21:19:11 -0500

    EntityFileRemote

---
+++ web/modules/custom/nerdcustom/src/Plugin/migrate/destination/EntityFileRemote.php
@@ -0,0 +1,66 @@
+<?php
+
+
+namespace Drupal\nerdcustom\Plugin\migrate\destination;
+
+use Drupal\file\Plugin\migrate\destination\EntityFile;
+use Drupal\migrate\Row;
+use Drupal\migrate\MigrateException;
+use Drupal\migrate\Plugin\MigrateIdMapInterface;
+print_r("
+
+asdfasdfafs
+
+
+");
+
+
+
+
+/**
+ * Every migration that uses this destination must have an optional
+ * dependency on the d6_file migration to ensure it runs first.
+ *
+ * @MigrateDestination(
+ *   id = "fileremote"
+ * )
+ */
+class EntityFileRemote extends EntityFile {
+
+  
+  /**
+   * Finds the entity type from configuration or plugin ID.
+   *
+   * @param string $plugin_id
+   *   The plugin ID.
+   *
+   * @return string
+   *   The entity type.
+   */
+  protected static function getEntityTypeId($plugin_id) {
+    return 'file';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function import(Row $row, array $old_destination_id_values = array()) {
+
+
+
+    $this->rollbackAction = MigrateIdMapInterface::ROLLBACK_DELETE;
+    $entity = $this->getEntity($row, $old_destination_id_values);
+    if (!$entity) {
+      throw new MigrateException('Unable to get entity');
+    }
+
+    $ids = $this->save($entity, $old_destination_id_values);
+    if (!empty($this->configuration['translations'])) {
+      $ids[] = $entity->language()->getId();
+    }
+    return $ids;
+  }
+
+
+
+}
