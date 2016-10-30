5ef6d435d53319353eda2a83194654cb8698b63b
Steve Persch <stevector@users.noreply.github.com>
2016-10-30 18:06:25 -0500

    comment

--- scripts/run-backstop.sh
+++ scripts/run-backstop.sh
@@ -2,7 +2,8 @@
 
 set -x
 
-sed -i -e "s/dev-nerdologues-d8/${TERMINUS_ENV}-nerdologues-d8/g" ~/nerdologues-d8/backstop.json
+    # Update the backstop.json file to use the multidev environment.
+    sed -i -e "s/dev-nerdologues-d8/${TERMINUS_ENV}-nerdologues-d8/g" ~/nerdologues-d8/backstop.json
 
 
     # install node dependencies
@@ -20,6 +21,5 @@ sed -i -e "s/dev-nerdologues-d8/${TERMINUS_ENV}-nerdologues-d8/g" ~/nerdologues-
     VISUAL_REGRESSION_RESULTS=$(npm run test)
 
     echo "${VISUAL_REGRESSION_RESULTS}"
-
 
     rsync  -rlvz   /home/ubuntu/nerdologues-d8/backstop_data $CIRCLE_ARTIFACTS
