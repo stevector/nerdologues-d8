e2f12f5cf1cf4a1b244ea5e574ebef0fdbf2ea03
Steve Persch <steve@stevepersch.com>
2016-11-04 09:25:53 -0500

    Code style fixes

--- web/modules/custom/nerdcustom/src/Plugin/migrate/process/NerdQuotesField.php
+++ web/modules/custom/nerdcustom/src/Plugin/migrate/process/NerdQuotesField.php
@@ -2,9 +2,7 @@
 
 /**
  * @file
- * A process plugin to set audio duration.
- *
- * Only nerdologues members should have bio pages.
+ * A process plugin to create quote paragraph entities from field collections.
  */
 
 namespace Drupal\nerdcustom\Plugin\migrate\process;
@@ -14,8 +12,9 @@ use Drupal\migrate\MigrateExecutableInterface;
 use Drupal\migrate\Row;
 use Drupal\Core\Database\Database;
 use Drupal\paragraphs\Entity\Paragraph;
+
 /**
- * A process plugin to set audio duration.
+ * A process plugin to create quote paragraph entities from field collections.
  *
  * @MigrateProcessPlugin(
  *   id = "nerd_quotes_field"
@@ -32,27 +31,31 @@ class NerdQuotesField extends ProcessPluginBase {
       ->select('field_data_field_body', 'fdfb');
     $query->join('field_data_field_int_start_time', 'fdfi', 'fdfi.entity_id=fdfb.entity_id');
 
-      $results = $query->fields('fdfb', ['field_body_value'])
-          ->fields('fdfi', ['field_int_start_time_value'])
+    $results = $query->fields('fdfb', ['field_body_value'])
+      ->fields('fdfi', ['field_int_start_time_value'])
       ->condition('fdfb.entity_id', $value['value'])
-        ->condition('fdfi.bundle', 'field_fc_quotes ')
-        ->condition('fdfb.bundle', 'field_fc_quotes ')->execute();
+      ->condition('fdfi.bundle', 'field_fc_quotes ')
+      ->condition('fdfb.bundle', 'field_fc_quotes ')->execute();
 
-      // There really should be only one value in this lloo
+    // There really should be only one value in this loop.
     foreach ($results as $result) {
-        if (!empty($result->field_body_value)) {
-            return $this->makeParagraph($result->field_body_value, $result->field_int_start_time_value);
-        }
+      if (!empty($result->field_body_value)) {
+        return $this->makeParagraph($result->field_body_value, $result->field_int_start_time_value);
+      }
     }
 
     return NULL;
   }
+
+  /**
+   * Helper method for making paragraph quote entities.
+   */
   protected function makeParagraph($body, $int_start = NULL) {
 
     $paragraph = Paragraph::create([
-        'type' => 'quotes',   // paragraph type machine name
-        'field_body_plain' => [   // paragraph's field machine name
-            'value' => $body,                  // body field value
+        'type' => 'quotes',
+        'field_body_plain' => [
+            'value' => $body,
         ],
         'field_int_start_time' => [
             'value' => $int_start,
