4bab918025f3ab0337e404c1875356c66251b800
Steve Persch <stevector@users.noreply.github.com>
2017-01-14 22:02:16 -0600

    Speeding up circle builds by running screenshotting only on master branch

--- scripts/run-backstop.sh
+++ scripts/run-backstop.sh
@@ -2,24 +2,32 @@
 
 set -x
 
-    # Update the backstop.json file to use the multidev environment.
-    sed -i -e "s/dev-nerdologues-d8/${TERMINUS_ENV}-nerdologues-d8/g" ~/nerdologues-d8/backstop.json
+# run screenshots only on master branch.
+#todo, detect some pattern in branch name or commit message to also trigger run.
+if [ "$CIRCLE_BRANCH" != "master" ]
+then
+    echo -e "Screenshots only run on master branch. Quitting script."
+fi
 
 
-    # install node dependencies
-    echo -e "\nRunning npm install..."
-    npm install
+# Update the backstop.json file to use the multidev environment.
+sed -i -e "s/dev-nerdologues-d8/${TERMINUS_ENV}-nerdologues-d8/g" ~/nerdologues-d8/backstop.json
 
-    # backstop visual regression
-    echo -e "\nRunning BackstopJS tests..."
 
-    cd node_modules/backstopjs
+# install node dependencies
+echo -e "\nRunning npm install..."
+npm install
 
-    npm run reference
-    # npm run test
+# backstop visual regression
+echo -e "\nRunning BackstopJS tests..."
 
-    VISUAL_REGRESSION_RESULTS=$(npm run test)
+cd node_modules/backstopjs
 
-    echo "${VISUAL_REGRESSION_RESULTS}"
+npm run reference
+# npm run test
 
-    rsync  -rlvz   /home/ubuntu/nerdologues-d8/backstop_data $CIRCLE_ARTIFACTS
+VISUAL_REGRESSION_RESULTS=$(npm run test)
+
+echo "${VISUAL_REGRESSION_RESULTS}"
+
+rsync  -rlvz   /home/ubuntu/nerdologues-d8/backstop_data $CIRCLE_ARTIFACTS
