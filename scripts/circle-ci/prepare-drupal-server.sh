026094215172581b114d7f13b71c365722efe99a
Steve Persch <stevector@users.noreply.github.com>
2017-01-17 07:11:34 -0600

    moving install

--- scripts/circle-ci/prepare-drupal-server.sh
+++ scripts/circle-ci/prepare-drupal-server.sh
@@ -33,7 +33,3 @@ sudo sed -e "s?%DOCROOT%?$DOCROOT?g" --in-place /etc/apache2/sites-available/def
 sudo sed -e "s?%SERVER%?$SERVER?g" --in-place /etc/apache2/sites-available/default
 sudo a2enmod rewrite
 sudo service apache2 restart
-
-# Install Drupal
-drush --yes --root=$DOCROOT site-install config_installer
-
