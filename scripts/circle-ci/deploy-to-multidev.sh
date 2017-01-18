d0025e0fed9f3ad4ba701e3c3d92a057977c0c9d
Steve Persch <steve@stevepersch.com>
2017-01-17 18:04:22 -0600

    terminus 1x

--- scripts/circle-ci/deploy-to-multidev.sh
+++ scripts/circle-ci/deploy-to-multidev.sh
@@ -9,20 +9,17 @@ git config user.email "stevepersch+circleci@gmail.com"
 git config user.name "Circle CI Automation"
 
 
-terminus site create-env --to-env=$TERMINUS_ENV --from-env=dev
-yes | terminus site clone-content --to-env=$TERMINUS_ENV --from-env=dev
+terminus build-env:create -n "$TERMINUS_SITE.dev" "$TERMINUS_ENV" --yes
 
-git remote add pantheon $(terminus site connection-info --field=git_url)
+git remote add pantheon $(terminus connection:info $SITE_ENV --field=git_url)
 git fetch pantheon
 git checkout -b $TERMINUS_ENV
 git pull pantheon $TERMINUS_ENV
 
-mkdir -p ~/terminus/plugins
-git clone https://github.com/greg-1-anderson/terminus-secrets-plugin  ~/terminus/plugins/terminus-secrets-plugin
 
-terminus site wake --site=nerdologues --env=migr-prep2
-export D7_MYSQL_URL=$(terminus site connection-info --site=nerdologues --env=migr-prep2 --field=mysql_url)
-terminus secrets set migrate_source_db__url $D7_MYSQL_URL
+terminus env:wake nerdologues.migr-prep2
+export D7_MYSQL_URL=$(terminus connection:info nerdologues.migr-prep2 --field=mysql_url)
+terminus secrets:set site.env $SITE_ENV migrate_source_db__url $D7_MYSQL_URL
 
 git push pantheon $TERMINUS_ENV -f
 # @todo Don't switch to sftp after
@@ -30,13 +27,13 @@ git push pantheon $TERMINUS_ENV -f
 terminus site set-connection-mode --mode=sftp
 # Send to dev null so that the generated admin password does not show.
 # Hiding all output might be overkill for accomplishing that goal.
-terminus drush "si -y config_installer" > /dev/null 2>&1
-terminus drush "ms"
-terminus drush "mi --all --feedback='50 items'"
-terminus drush "ms"
+terminus drush $SITE_ENV -- si -y config_installer > /dev/null 2>&1
+terminus drush $SITE_ENV -- ms
+terminus drush $SITE_ENV -- mi --all --feedback='50 items'
+terminus drush $SITE_ENV -- ms
 
 # Create a drush alias file so that Behat tests can be executed against Pantheon.
-terminus sites aliases
+terminus aliases
 # Drush Behat driver fails without this option.
 echo "\$options['strict'] = 0;" >> ~/.drush/pantheon.aliases.drushrc.php
 # Update Behat Params so that migration tests can be run against Pantheon.
