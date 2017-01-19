54dcafbcf0de910cdea638b9d0c74a1ea1f83d13
Steve Persch <steve@stevepersch.com>
2017-01-18 21:25:55 -0600

    command fix

--- scripts/circle-ci/deploy-to-multidev.sh
+++ scripts/circle-ci/deploy-to-multidev.sh
@@ -27,9 +27,7 @@ git push pantheon $TERMINUS_ENV -f
 terminus connection:set $SITE_ENV sftp
 # Send to dev null so that the generated admin password does not show.
 # Hiding all output might be overkill for accomplishing that goal.
-terminus drush $SITE_ENV -- si -y config_installer
-
- # /dev/null 2>&1
+terminus drush $SITE_ENV -- si -y config_installer > /dev/null 2>&1
 
 
 
