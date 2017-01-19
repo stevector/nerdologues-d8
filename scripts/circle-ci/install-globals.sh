3ddd35344fcc29d205dfa4a2ce5af65938e13563
Steve Persch <steve@stevepersch.com>
2017-01-18 20:05:48 -0600

    install

--- scripts/circle-ci/install-globals.sh
+++ scripts/circle-ci/install-globals.sh
@@ -5,7 +5,7 @@ set -x
 composer global require -n "consolidation/cgr"
 composer global require -n "hirak/prestissimo:^0.3"
 cgr "drush/drush:~8"
-curl -O https://raw.githubusercontent.com/pantheon-systems/terminus-installer/master/builds/installer.phar && php installer.phar && which terminus
+curl -O https://raw.githubusercontent.com/pantheon-systems/terminus-installer/master/builds/installer.phar && php installer.phar install
 terminus --version
 mkdir -p ~/.terminus/plugins
 # todo, do I really want the dev-reusue-multidev branch?
