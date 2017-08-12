207c7e855811204ac965aa10fcb3af8b3d28620a
Steve Persch <stevector@users.noreply.github.com>
2017-08-11 21:30:32 -0500

    Update deploy-to-multidev.sh

--- scripts/circle-ci/deploy-to-multidev.sh
+++ scripts/circle-ci/deploy-to-multidev.sh
@@ -37,7 +37,6 @@ terminus drush $SITE_ENV -- ms
 terminus aliases
 # Drush Behat driver fails without this option.
 echo "\$options['strict'] = 0;" >> ~/.drush/pantheon.aliases.drushrc.php
-
 # Update Behat Params so that migration tests can be run against Pantheon.
 export BEHAT_PARAMS='{"extensions" : {"Behat\\MinkExtension" : {"base_url" : "http://'$TERMINUS_ENV'-'$TERMINUS_SITE'.pantheonsite.io/"}, "Drupal\\DrupalExtension" : {"drush" :   {  "alias":  "@pantheon.'$TERMINUS_SITE'.'$TERMINUS_ENV'" }}}}'
 # Make sure the site is accessible over the web before making requests to it with Behat.
