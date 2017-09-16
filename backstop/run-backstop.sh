0ebac896c7b0dbdc2d81b78b8e658d4e82c6b295
Steve Persch <steve@stevepersch.com>
2017-09-16 15:52:20 -0500

    fail screenshot test

--- backstop/run-backstop.sh
+++ backstop/run-backstop.sh
@@ -13,7 +13,19 @@ sed -i -e "s/dev-${TERMINUS_SITE}/${TERMINUS_ENV}-${TERMINUS_SITE}/g" ~/$CIRCLE_
 backstop reference
 VISUAL_REGRESSION_RESULTS=$(backstop test || echo 'true')
 
-rsync -rlvz backstop_data $CIRCLE_ARTIFACTS
+if [[ ${VISUAL_REGRESSION_RESULTS} == *"Mismatch errors found"* ]]
+then
+    # Visual Regression Failed. Get Visual Difference Image
+    echo -e "\nVisual regression tests failed!"
+    comment="### Visual regression report (failed):"
+    EXIT=1
+else
+    echo -e "\nVisual regression tests passed"
+   comment="### Visual regression report (passed):"
+   EXIT=0
+fi
+
+#rsync -rlvz backstop_data $CIRCLE_ARTIFACTS
 
 artifact_base_url="https://circleci.com/api/v1.1/project/github/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/$CIRCLE_BUILD_NUM/artifacts/0$CIRCLE_ARTIFACTS"
 
@@ -21,7 +33,9 @@ diff_image=$(find * | grep png | grep diff | head -n 1)
 diff_image_url=$artifact_base_url/$diff_image
 report_url=$artifact_base_url/backstop_data/html_report/index.html
 report_link="[![Visual report]($diff_image_url)]($report_url)"
-comment="### Visual regression report:"
+
 
 token="$(composer config --global github-oauth.github.com)"
 curl -d '{ "body": "'"$comment\\n\\n$report_link"'" }' -X POST https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/commits/$CIRCLE_SHA1/comments?access_token=$token
+
+exit $EXIT
