a3638527b8ba641879f7831c1ced67b28b9f5325
Steve Persch <stevector@users.noreply.github.com>
2017-09-17 15:30:59 -0500

    Update install-globals.sh

--- scripts/circle-ci/install-globals.sh
+++ scripts/circle-ci/install-globals.sh
@@ -10,7 +10,7 @@ composer global require -n "consolidation/cgr"
 composer global require -n "hirak/prestissimo:^0.3"
 cgr "drush/drush:~8"
 
-git clone --branch master https://github.com/pantheon-systems/terminus.git ~/terminus		 +/usr/bin/env COMPOSER_BIN_DIR=$HOME/bin composer -n --working-dir=$HOME require pantheon-systems/terminus "^1"
+git clone --branch master https://github.com/pantheon-systems/terminus.git ~/terminus
 cd ~/terminus && composer install
 
 terminus --version
