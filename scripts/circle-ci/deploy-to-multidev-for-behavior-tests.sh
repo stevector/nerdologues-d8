6019b78c9aae296a14d967db3c2760e71c3e5a34
Steve Persch <steve@stevepersch.com>
2017-11-05 10:09:48 -0600

    splitting circle2

--- scripts/circle-ci/deploy-to-multidev-for-behavior-tests.sh
+++ scripts/circle-ci/deploy-to-multidev-for-behavior-tests.sh
@@ -5,16 +5,12 @@
 
 set -ex
 
-
-
 # Create a drush alias file so that Behat tests can be executed against Pantheon.
 terminus aliases
 # Drush Behat driver fails without this option.
 echo "\$options['strict'] = 0;" >> ~/.drush/pantheon.aliases.drushrc.php
 # Update Behat Params so that migration tests can be run against Pantheon.
 export BEHAT_PARAMS='{"extensions" : {"Behat\\MinkExtension" : {"base_url" : "http://'$TERMINUS_ENV'-'$TERMINUS_SITE'.pantheonsite.io/"}, "Drupal\\DrupalExtension" : {"drush" :   {  "alias":  "@pantheon.'$TERMINUS_SITE'.'$TERMINUS_ENV'" }}}}'
-# Make sure the site is accessible over the web before making requests to it with Behat.
-
 
 
 # delete old multidevs before making a new one
@@ -27,32 +23,13 @@ terminus -n build:env:create "$TERMINUS_SITE.dev" "$TERMINUS_ENV" --yes --notify
 # if the local Drupal install is broken.
 sudo cp scripts/circle-ci/settings.cirlceci.php web/sites/default/settings.local.php
 
-# @todo Don't switch to sftp after
-# https://www.drupal.org/node/2156401 lands
 terminus connection:set $SITE_ENV sftp
 # Send to dev null so that the generated admin password does not show.
 # Hiding all output might be overkill for accomplishing that goal.
 terminus drush $SITE_ENV -- si -y config_installer > /dev/null 2>&1
 
 
-
-terminus env:wake nerdologues.$D7_ENV
-export D7_MYSQL_URL=$(terminus connection:info nerdologues.$D7_ENV --field=mysql_url)
-terminus secrets:set $SITE_ENV migrate_source_db__url $D7_MYSQL_URL
-
-terminus drush $SITE_ENV -- si -y config_installer > /dev/null 2>&1
-
-
-
-
-terminus drush $SITE_ENV -- ms
-terminus drush $SITE_ENV -- mi --all --feedback='50 items'
-terminus drush $SITE_ENV -- ms
-
-
-
 {
-
   terminus drush $SITE_ENV -- user-create   $BEHAT_USER_ADMIN  --password=$BEHAT_PASS_ADMIN
   terminus drush $SITE_ENV -- user-add-role administrator   $BEHAT_USER_ADMIN
   terminus drush $SITE_ENV -- user-create   $BEHAT_USER_CONTENT_ADMIN  --password=$BEHAT_PASS_CONTENT_ADMIN
@@ -61,9 +38,9 @@ terminus drush $SITE_ENV -- ms
 } &> /dev/null
 
 
-
-
 curl http://$TERMINUS_ENV-$TERMINUS_SITE.pantheonsite.io/
+./vendor/bin/behat --config=tests/behat/behat-pantheon.yml --suite=clickdriving --strict
 
-./vendor/bin/behat --config=tests/behat/behat-pantheon.yml --suite=migration --strict
-./vendor/bin/behat --config=tests/behat/behat-pantheon.yml --suite=dataentry --strict
+
+
+
--- scripts/circle-ci/install-globals.sh
+++ scripts/circle-ci/install-globals.sh
@@ -5,6 +5,8 @@ set -x
 git config user.email "stevepersch+circleci@gmail.com"
 git config user.name "Circle CI Automation"
 
+mkdir -p $HOME/.ssh && echo "StrictHostKeyChecking no" >> "$HOME/.ssh/config"
+
 {
   composer config --global github-oauth.github.com $GITHUB_TOKEN
 } &> /dev/null
