549fa7bc76b03fac0e92f097af03df89b6db425d
Steve Persch <steve@stevepersch.com>
2017-11-08 20:31:49 -0600

    fixing paths

--- scripts/circle-ci/deploy-to-multidev.sh
+++ scripts/circle-ci/deploy-to-multidev.sh
@@ -5,18 +5,11 @@
 
 set -ex
 
-
-
-
 # delete old multidevs before making a new one
+# @todo This command is not deleting multidevs in the right order,
+# nor retain the correct number.
 #terminus -n build:env:delete:ci "$TERMINUS_SITE" --keep=8 --yes
 terminus -n build:env:create "$TERMINUS_SITE.dev" "$TERMINUS_ENV" --yes --notify="$NOTIFY"
-# Create a drush alias file so that Behat tests can be executed against Pantheon.
-terminus aliases
-# Drush Behat driver fails without this option.
-echo "\$options['strict'] = 0;" >> ~/.drush/pantheon.aliases.drushrc.php
-
-
 
 # @todo Don't switch to sftp after
 # https://www.drupal.org/node/2156401 lands
@@ -24,5 +17,3 @@ terminus connection:set $SITE_ENV sftp
 # Send to dev null so that the generated admin password does not show.
 # Hiding all output might be overkill for accomplishing that goal.
 terminus drush $SITE_ENV -- si -y config_installer > /dev/null 2>&1
-
-
--- scripts/circle-ci/run-migration.sh
+++ scripts/circle-ci/run-migration.sh
@@ -5,12 +5,9 @@
 
 set -ex
 
-
-
 terminus env:wake nerdologues.$D7_ENV
 export D7_MYSQL_URL=$(terminus connection:info nerdologues.$D7_ENV --field=mysql_url)
 terminus secrets:set $SITE_ENV migrate_source_db__url $D7_MYSQL_URL
-
 
 terminus drush $SITE_ENV -- ms
 terminus drush $SITE_ENV -- mi --all --feedback='50 items'
