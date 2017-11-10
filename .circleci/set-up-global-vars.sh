2e39817767c9758ef4992edb0dce0e6b7882113a
Steve Persch <steve@stevepersch.com>
2017-11-10 16:06:45 -0600

    Fixing global vars for backstop

--- .circleci/set-up-global-vars.sh
+++ .circleci/set-up-global-vars.sh
@@ -24,14 +24,13 @@ set -ex
 CIRCLE_ARTIFACTS_DIR='/tmp/artifacts'
 mkdir -p $CIRCLE_ARTIFACTS_DIR
 
-
-echo 'export D7_ENV=migr-prep3'
-echo 'export MIGRATION_SOURCE_URL="http://$D7_ENV-nerdologues.pantheonsite.io"'
+echo 'export TERMINUS_ENV=${TERMINUS_ENV}' >> $BASH_ENV
+echo 'export D7_ENV=migr-prep3' >> $BASH_ENV
+echo 'export MIGRATION_SOURCE_URL="http://$D7_ENV-nerdologues.pantheonsite.io"' >> $BASH_ENV
 
 echo 'export PANTHEON_DEV_SITE_URL=https://dev-${TERMINUS_SITE}.pantheonsite.io' >> $BASH_ENV
 echo 'export PANTHEON_SITE_URL=https://${TERMINUS_ENV}-${TERMINUS_SITE}.pantheonsite.io' >> $BASH_ENV
 echo 'CIRCLE_ARTIFACTS_DIR="/tmp/artifacts"' >> $BASH_ENV
 echo 'export CIRCLE_ARTIFACTS_URL=${CIRCLE_BUILD_URL}/artifacts/$CIRCLE_NODE_INDEX/artifacts' >> $BASH_ENV
 echo 'export PR_NUMBER=${CIRCLE_PULL_REQUEST##*/}' >> $BASH_ENV
-echo 'export TERMINUS_ENV=${TERMINUS_ENV}' >> $BASH_ENV
 source $BASH_ENV
