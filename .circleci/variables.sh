f83c6d03b8aaa4ce38194ad165192324316f1068
Steve Persch <steve@stevepersch.com>
2017-11-10 22:53:46 -0600

    Deleting unused script

--- .circleci/variables.sh
+++ .circleci/variables.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+
+set -x
+
+git config user.email "stevepersch+circleci@gmail.com"
+git config user.name "Circle CI Automation"
+
+mkdir -p $HOME/.ssh && echo "StrictHostKeyChecking no" >> "$HOME/.ssh/config"
+
+{
+  composer config --global github-oauth.github.com $GITHUB_TOKEN
+} &> /dev/null
+
+
+
+{
+ terminus auth:login -n --machine-token="$TERMINUS_TOKEN"
+} &> /dev/null
+
+terminus whoami
+
+
 (
   echo 'export D7_ENV=migr-prep3'
   echo 'export TERMINUS_ENV=ci-$CIRCLE_BUILD_NUM'
