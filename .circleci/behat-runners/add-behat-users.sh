6f6d323eb24943041d270805bcdd2fc13ff24070
Steve Persch <steve@stevepersch.com>
2017-11-10 18:47:48 -0600

    Fixing global vars for behat

--- .circleci/behat-runners/add-behat-users.sh
+++ .circleci/behat-runners/add-behat-users.sh
@@ -5,8 +5,8 @@ echo "TEST"
 
 echo $SITE_ENV
 
-  terminus drush $TERMINUS_SITE.$TERMINNUS_ENV -- user-create   $BEHAT_USER_ADMIN  --password=$BEHAT_PASS_ADMIN
-  terminus drush $TERMINUS_SITE.$TERMINNUS_ENV -- user-add-role administrator   $BEHAT_USER_ADMIN
-  terminus drush $TERMINUS_SITE.$TERMINNUS_ENV -- user-create   $BEHAT_USER_CONTENT_ADMIN  --password=$BEHAT_PASS_CONTENT_ADMIN
-  terminus drush $TERMINUS_SITE.$TERMINNUS_ENV -- user-add-role content_administrator   $BEHAT_USER_CONTENT_ADMIN
+  terminus drush $TERMINUS_SITE.$TERMINUS_ENV -- user-create   $BEHAT_USER_ADMIN  --password=$BEHAT_PASS_ADMIN
+  terminus drush $TERMINUS_SITE.$TERMINUS_ENV -- user-add-role administrator   $BEHAT_USER_ADMIN
+  terminus drush $TERMINUS_SITE.$TERMINUS_ENV -- user-create   $BEHAT_USER_CONTENT_ADMIN  --password=$BEHAT_PASS_CONTENT_ADMIN
+  terminus drush $TERMINUS_SITE.$TERMINUS_ENV -- user-add-role content_administrator   $BEHAT_USER_CONTENT_ADMIN
 
