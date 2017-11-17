52c115c307521e86bd908df4c6a1697e3cc97a7d
Steve Persch <steve@stevepersch.com>
2017-11-16 21:41:35 -0600

    debugging failing aliases

--- .circleci/behat-runners/behat-wrapper.sh
+++ .circleci/behat-runners/behat-wrapper.sh
@@ -6,6 +6,7 @@
 set -ex
 
 terminus aliases
+cat ~/.drush/pantheon.aliases.drushrc.php
 # Update Behat Params so that migration tests can be run against Pantheon.
 export BEHAT_PARAMS='{"extensions" : {"Behat\\MinkExtension" : {"base_url" : "http://'$TERMINUS_ENV'-'$TERMINUS_SITE'.pantheonsite.io/"}, "Drupal\\DrupalExtension" : {"drush" :   {  "alias":  "@pantheon.'$TERMINUS_SITE'.'$TERMINUS_ENV'" }}}}'
 # Make sure the site is accessible over the web before making requests to it with Behat.
