04674725f665a34c13cd370dce4ceef62dd83611
Steve Persch <stevector@users.noreply.github.com>
2017-12-23 10:20:28 -0600

    [ci-skip] changing from local drush to terminus for behat

--- .circleci/behat-runners/behat-wrapper.sh
+++ .circleci/behat-runners/behat-wrapper.sh
@@ -5,10 +5,8 @@
 
 set -ex
 
-terminus aliases
-cat ~/.drush/pantheon.aliases.drushrc.php
 # Update Behat Params so that migration tests can be run against Pantheon.
-export BEHAT_PARAMS='{"extensions" : {"Behat\\MinkExtension" : {"base_url" : "http://'$TERMINUS_ENV'-'$TERMINUS_SITE'.pantheonsite.io/"}, "Drupal\\DrupalExtension" : {"drush" :   {  "alias":  "@pantheon.'$TERMINUS_SITE'.'$TERMINUS_ENV'" }}}}'
+export BEHAT_PARAMS='{"extensions" : {"Behat\\MinkExtension" : {"base_url" : "http://'$TERMINUS_ENV'-'$TERMINUS_SITE'.pantheonsite.io/"}}}'
 # Make sure the site is accessible over the web before making requests to it with Behat.
 curl http://$TERMINUS_ENV-$TERMINUS_SITE.pantheonsite.io/
 
