7c439cd53947f35c8e380963d1a35de633b0e8be
Steve Persch <steve@stevepersch.com>
2018-01-19 09:01:37 -0600

    log in

--- .circleci/merge-to-master.sh
+++ .circleci/merge-to-master.sh
@@ -2,6 +2,12 @@
 
 set -ex
 
+{
+ terminus auth:login -n --machine-token="$TERMINUS_TOKEN"
+} &> /dev/null
+
+terminus whoami
+
 git remote add pantheon $(terminus connection:info $TERMINUS_SITE.dev --field=git_url)
 terminus -n build:env:merge "$TERMINUS_SITE.$TERMINUS_ENV"
 terminus env:clone-content "$TERMINUS_SITE.$TERMINUS_ENV" dev --yes
