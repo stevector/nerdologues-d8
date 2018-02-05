b56cf48c4b8e4fe42025bbf37225413796fbc8d4
Steve Persch <steve@stevepersch.com>
2018-02-04 20:53:43 -0600

    [ci skip] removing commented out code

--- .circleci/deploy/deploy-to-multidev.sh
+++ .circleci/deploy/deploy-to-multidev.sh
@@ -10,10 +10,4 @@ set -ex
 # nor retain the correct number.
 #terminus -n build:env:delete:ci "$TERMINUS_SITE" --keep=8 --yes
 
-
 terminus -n build:env:create "$TERMINUS_SITE.dev" "$TERMINUS_ENV" --yes  --clone-content
-
-##### terminus connection:set $SITE_ENV sftp
-# Send to dev null so that the generated admin password does not show.
-# Hiding all output might be overkill for accomplishing that goal.
-##### terminus drush $SITE_ENV -- si -y config_installer > /dev/null 2>&1
