1d293bc1c94fc71b9dac83519aeea08eaadba43a
Steve Persch <steve@stevepersch.com>
2018-05-25 21:56:31 -0500

    delete migration job. only delete old multidev from build job

--- .circleci/deploy/deploy-to-multidev.sh
+++ .circleci/deploy/deploy-to-multidev.sh
@@ -7,5 +7,4 @@ set -ex
 
 # delete old multidevs before making a new one
 
-terminus env:list --field=id $TERMINUS_SITE | grep "^ci-" | cut -c 4-  | grep -Eo '[0-9]{1,9}' | sort --numeric-sort --reverse | sed 1,7d | xargs -n1 -I ENV terminus env:delete --yes $TERMINUS_SITE.ci-ENV
 terminus -n build:env:create "$TERMINUS_SITE.dev" "$TERMINUS_ENV" --yes  --clone-content
