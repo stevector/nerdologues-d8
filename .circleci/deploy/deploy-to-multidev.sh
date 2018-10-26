d54765dcbca944f1ce1fec1016636c9999bfa1cd
Steve Persch <stevector@users.noreply.github.com>
2018-10-25 19:00:06 -0700

    Core update 861 (#273)
    * drupal core 8.6.1
    
    * Update installer
    
    * getting unit tests working
    
    * composer.lock
    
    * changing composer name
    
    * composer updates
    
    * composer updates
    
    * patch update
    
    * patch update
    
    * patch update
    
    * patch update
    
    * config updates
    
    * remove profile from settings.php
    
    * Update system.site.yml
    
    * Update settings.php
    
    * Stevector patch 1 (#274)
    
    * Update remote_stream_wrapper_require.patch
    
    * Update remote_stream_wrapper_require.patch
    
    * switch to drupal console for installs [ci skip]
    
    * Create config.yml
    
    * adding a few cache rebuilds and wrapping multidev deletion
    
    * events page update
    
    * Update deploy-to-multidev.sh
    
    * Update deploy-to-multidev.sh

--- .circleci/deploy/deploy-to-multidev.sh
+++ .circleci/deploy/deploy-to-multidev.sh
@@ -6,11 +6,21 @@
 set -ex
 
 # delete old multidevs before making a new one
-terminus env:list --field=id $TERMINUS_SITE | grep "^ci-" | cut -c 4-  | grep -Eo '[0-9]{1,9}' | sort --numeric-sort --reverse | sed 1,7d | xargs -n1 -I ENV terminus env:delete --yes $TERMINUS_SITE.ci-ENV
+# Only run on even numbered builds to reduce the likelihood of this
+# script running on two containers at the same time and causing
+# errors.
+if [ $(($CIRCLE_BUILD_NUM%2)) -eq 0 ];
+then
+   terminus env:list --field=id $TERMINUS_SITE | grep "^ci-" | cut -c 4-  | grep -Eo '[0-9]{1,9}' | sort --numeric-sort --reverse | sed 1,7d | xargs -n1 -I ENV terminus env:delete --yes $TERMINUS_SITE.ci-ENV
+fi
+
 
 terminus -n build:env:create "$TERMINUS_SITE.dev" "$TERMINUS_ENV" --yes
 
 terminus connection:set $SITE_ENV sftp
 # Send to dev null so that the generated admin password does not show.
 # Hiding all output might be overkill for accomplishing that goal.
-terminus drush $SITE_ENV -- si -y config_installer > /dev/null 2>&1
+#terminus drush $SITE_ENV -- si -y config_installer > /dev/null 2>&1
+terminus --quiet drupal $SITE_ENV -- site:install   --force --no-interaction
+terminus --quiet drupal $SITE_ENV -- config:import --no-interaction
+terminus drush $SITE_ENV -- cr
--- .circleci/deploy/run-migration.sh
+++ .circleci/deploy/run-migration.sh
@@ -9,7 +9,9 @@ terminus env:wake nerdologues.$D7_ENV
 export D7_MYSQL_URL=$(terminus connection:info nerdologues.$D7_ENV --field=mysql_url)
 terminus secrets:set $SITE_ENV migrate_source_db__url $D7_MYSQL_URL
 
+terminus drush $SITE_ENV -- cr
 terminus drush $SITE_ENV -- ms
 terminus drush $SITE_ENV -- mi --all --feedback='50 items'
+terminus drush $SITE_ENV -- cr
 terminus drush $SITE_ENV -- ms
 
