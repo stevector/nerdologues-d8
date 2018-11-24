b2b79302544489ed50abc4d5caf3470a93fed85d
Steve Persch <stevector@users.noreply.github.com>
2018-11-24 00:35:48 -0600

    merge to master clean up

--- .circleci/merge-to-master.sh
+++ .circleci/merge-to-master.sh
@@ -10,5 +10,5 @@ terminus whoami
 
 git remote add pantheon $(terminus connection:info $TERMINUS_SITE.dev --field=git_url)
 terminus -n build:env:merge "$TERMINUS_SITE.$TERMINUS_ENV"
-terminus env:clone-content "$TERMINUS_SITE.$TERMINUS_ENV" dev --yes
+
 terminus env:clear-cache "$TERMINUS_SITE.$TERMINUS_ENV"
