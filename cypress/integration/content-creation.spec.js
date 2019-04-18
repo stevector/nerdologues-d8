b372f533dc3139ca88d0f59e24ad56ab4bb3325e
Steve Persch <stevector@users.noreply.github.com>
2019-04-18 17:42:19 -0500

    Adding Cypress tests (#309)
    * remove migration check job
    
    * remove migration config
    
    * Adding cypress in package
    
    * cypress in git ignore
    
    * cypress.json
    
    * Adding cypress boilerplate
    
    * limit merge job to master
    
    * limit merge job to master
    
    * Trying to run cypress in CircleCI
    
    * setting cypress variables
    
    * troubleshooting variables
    
    * update-container
    
    * changing composer caching
    
    * changing composer caching
    
    * php deprecation fix
    
    * php deprecation fix
    
    * comment fix
    
    * bring back ci cache
    
    * white space
    
    * Update content-creation.spec.js
    
    * fixing permissions
    
    * Run cypress in Nightly. Do not run merge in nightly. Require cypress for merge
    
    * ci fix

---
+++ cypress/integration/content-creation.spec.js
@@ -0,0 +1,19 @@
+describe('Hello world ', function(){
+  context('Make some nodes', function(){
+    it('Logs in', function(){
+
+      cy.visit('user');
+      cy.get('input[name=name]').type(Cypress.env('USER_CONTENT_ADMIN'))
+      cy.get('input[name=pass]').type(Cypress.env('PASS_CONTENT_ADMIN') + '{enter}')
+
+      cy.visit('node/add/blog_post');
+      var d = new Date();
+      var n = d.getTime();
+      const title = 'Hello World ' + n;
+      cy.get('input[name="title[0][value]"]').type(title, {force: true})
+      cy.get('#edit-submit').click();
+      cy.wait(500);
+      cy.get('h1.page-title').contains(title);
+    })
+  })
+})
