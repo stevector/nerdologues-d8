039ca96ebefcb2e990ff604cd997e7b4724634cf
Steve Persch <steve.persch@pantheon.io>
2019-04-18 22:20:26 -0500

    CI cleanup

--- backstop/run-backstop.sh
+++ backstop/run-backstop.sh
@@ -2,8 +2,7 @@
 
 set -ex
 
-
 backstop reference --config=backstop-config.js
-VISUAL_REGRESSION_RESULTS=$(backstop test --config=backstop-config.js || echo 'true')
+backstop test --config=backstop-config.js
 
 mv backstop_data $CIRCLE_ARTIFACTS_DIR/backstop_data
