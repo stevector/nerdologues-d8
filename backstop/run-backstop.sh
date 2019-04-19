4b788747261926386ffc52e4729646ce418de771
Steve Persch <steve.persch@pantheon.io>
2019-04-19 12:51:08 -0500

    mkdir artifacts

--- backstop/run-backstop.sh
+++ backstop/run-backstop.sh
@@ -3,8 +3,11 @@
 set -ex
 
 CIRCLE_ARTIFACTS_DIR="/tmp/artifacts"
+mkdir -p $CIRCLE_ARTIFACTS_DIR
 
 backstop reference --config=backstop-config.js
 backstop test --config=backstop-config.js
+
+ls -al
 
 mv backstop_data $CIRCLE_ARTIFACTS_DIR/backstop_data
