f815c626302599c5b1eb3f522b4cc25bbd7e8854
Steve Persch <steve.persch@pantheon.io>
2019-05-22 21:07:12 -0500

    Update prepare-for-pantheon

--- scripts/composer/ScriptHandler.php
+++ scripts/composer/ScriptHandler.php
@@ -8,16 +8,19 @@
 namespace DrupalProject\composer;
 
 use Composer\Script\Event;
-use Composer\Semver\Comparator;
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
 
-class ScriptHandler {
+class ScriptHandler
+{
 
-  protected static function getDrupalRoot($project_root) {
-    return $project_root . '/web';
+  protected static function getDrupalRoot($project_root)
+  {
+    return $project_root .  '/web';
   }
 
-  public static function createRequiredFiles(Event $event) {
+  public static function createRequiredFiles(Event $event)
+  {
     $fs = new Filesystem();
     $root = static::getDrupalRoot(getcwd());
 
@@ -35,20 +38,6 @@ class ScriptHandler {
       }
     }
 
-    // Prepare the settings file for installation
-    if (!$fs->exists($root . '/sites/default/settings.php') and $fs->exists($root . '/sites/default/default.settings.php')) {
-      $fs->copy($root . '/sites/default/default.settings.php', $root . '/sites/default/settings.php');
-      $fs->chmod($root . '/sites/default/settings.php', 0666);
-      $event->getIO()->write("Create a sites/default/settings.php file with chmod 0666");
-    }
-
-    // Prepare the services file for installation
-    if (!$fs->exists($root . '/sites/default/services.yml') and $fs->exists($root . '/sites/default/default.services.yml')) {
-      $fs->copy($root . '/sites/default/default.services.yml', $root . '/sites/default/services.yml');
-      $fs->chmod($root . '/sites/default/services.yml', 0666);
-      $event->getIO()->write("Create a sites/default/services.yml file with chmod 0666");
-    }
-
     // Create the files directory with chmod 0777
     if (!$fs->exists($root . '/sites/default/files')) {
       $oldmask = umask(0);
@@ -58,41 +47,37 @@ class ScriptHandler {
     }
   }
 
-  /**
-   * Checks if the installed version of Composer is compatible.
-   *
-   * Composer 1.0.0 and higher consider a `composer install` without having a
-   * lock file present as equal to `composer update`. We do not ship with a lock
-   * file to avoid merge conflicts downstream, meaning that if a project is
-   * installed with an older version of Composer the scaffolding of Drupal will
-   * not be triggered. We check this here instead of in drupal-scaffold to be
-   * able to give immediate feedback to the end user, rather than failing the
-   * installation after going through the lengthy process of compiling and
-   * downloading the Composer dependencies.
-   *
-   * @see https://github.com/composer/composer/pull/5035
-   */
-  public static function checkComposerVersion(Event $event) {
-    $composer = $event->getComposer();
-    $io = $event->getIO();
-
-    $version = $composer::VERSION;
-
-    // The dev-channel of composer uses the git revision as version number,
-    // try to the branch alias instead.
-    if (preg_match('/^[0-9a-f]{40}$/i', $version)) {
-      $version = $composer::BRANCH_ALIAS_VERSION;
+  // This is called by the QuickSilver deploy hook to convert from
+  // a 'lean' repository to a 'fat' repository. This should only be
+  // called when using this repository as a custom upstream, and
+  // updating it with `terminus composer <site>.<env> update`. This
+  // is not used in the GitHub PR workflow.
+  public static function prepareForPantheon()
+  {
+    // Get rid of any .git directories that Composer may have added.
+    // n.b. Ideally, there are none of these, as removing them may
+    // impair Composer's ability to update them later. However, leaving
+    // them in place prevents us from pushing to Pantheon.
+    $dirsToDelete = [];
+    $finder = new Finder();
+    foreach (
+      $finder
+        ->directories()
+        ->in(getcwd())
+        ->ignoreDotFiles(false)
+        ->ignoreVCS(false)
+        ->depth('> 0')
+        ->name('.git')
+      as $dir) {
+      $dirsToDelete[] = $dir;
     }
+    $fs = new Filesystem();
+    $fs->remove($dirsToDelete);
 
-    // If Composer is installed through git we have no easy way to determine if
-    // it is new enough, just display a warning.
-    if ($version === '@package_version@' || $version === '@package_branch_alias_version@') {
-      $io->writeError('<warning>You are running a development version of Composer. If you experience problems, please update Composer to the latest stable version.</warning>');
-    }
-    elseif (Comparator::lessThan($version, '1.0.0')) {
-      $io->writeError('<error>Drupal-project requires Composer version 1.0.0 or higher. Please update your Composer before continuing</error>.');
-      exit(1);
-    }
+    // Fix up .gitignore: remove everything above the "::: cut :::" line
+    $gitignoreFile = getcwd() . '/.gitignore';
+    $gitignoreContents = file_get_contents($gitignoreFile);
+    $gitignoreContents = preg_replace('/.*::: cut :::*/s', '', $gitignoreContents);
+    file_put_contents($gitignoreFile, $gitignoreContents);
   }
-
 }
